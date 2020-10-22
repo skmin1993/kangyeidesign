9b5b58856dc9b2596abeb247d669fb913986f11c
skmin1993 <skmin95@gmail.com>
2020-10-22 18:25:44 +0800

    git push

---
+++ config/deploy.rb
@@ -0,0 +1,55 @@
+# config valid for current version and patch releases of Capistrano
+lock "~> 3.14.1"
+
+set :application, "kangyeidesign"
+set :repo_url, "git@github.com:skmin1993/kangyeidesign.git"
+
+
+set :pg_without_sudo, false
+set :pg_host, 'localhost'
+set :pg_database, 'appname_production'
+set :pg_username, 'username'
+set :pg_password, ENV['DATABASE_USER_PASSWORD'] # Example is an ENV value, but you can use a string instead
+set :pg_ask_for_password, true # Prompts user for password on execution of setup
+set :pg_generate_random_password, true # Generates a random password on each setup
+# Deploy to the user's home directory
+set :deploy_to, "/home/deploy/#{fetch :application}"
+
+append :linked_dirs, 'log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', '.bundle', 'public/system', 'public/uploads'
+
+# Only keep the last 5 releases to save disk space
+set :keep_releases, 5
+
+# Default branch is :master
+# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+
+# Default deploy_to directory is /var/www/my_app_name
+# set :deploy_to, "/var/www/my_app_name"
+
+# Default value for :format is :airbrussh.
+# set :format, :airbrussh
+
+# You can configure the Airbrussh format using :format_options.
+# These are the defaults.
+# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
+
+# Default value for :pty is false
+# set :pty, true
+
+# Default value for :linked_files is []
+# append :linked_files, "config/database.yml"
+
+# Default value for linked_dirs is []
+# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+
+# Default value for default_env is {}
+# set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+# Default value for local_user is ENV['USER']
+# set :local_user, -> { `git config user.name`.chomp }
+
+# Default value for keep_releases is 5
+# set :keep_releases, 5
+
+# Uncomment the following to require manually verifying the host key before first deploy.
+# set :ssh_options, verify_host_key: :secure
