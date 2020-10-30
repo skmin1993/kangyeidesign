70dfacd564761204a2745e4e41e0742991e595ad
skmin1993 <skmin95@gmail.com>
2020-10-31 00:07:13 +0800

    amendement with added logo
    added logoo

--- config/routes.rb
+++ config/routes.rb
@@ -4,12 +4,19 @@ Rails.application.routes.draw do
  
   root 'welcome#index'
 
+  resources :about, only: [:index] do
+  end
+
+  resources :contact_us, only: [:index] do
+  end
 
   resources :product, only: [:index] do
      collection do
       get :packaging
       get :menu_book
+      get :company
     end
   end
 
 end
+
